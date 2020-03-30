98c6f69131c258ed645aab7bfed89123bc4638fe
Pratk Patil <contactme.pratikpatil@gmail.com>
2020-03-30 11:29:07 +0530

    Initial Commit

---
+++ TestAppleLogInPratik/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestAppleLogInPratik
+//
+//  Created by Apple on 30/03/20.
+//  Copyright © 2020 Pratik Patil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
